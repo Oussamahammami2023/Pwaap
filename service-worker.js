d5b0770ed046390d3fd12d605cc7c6937927d0e4
CME2024 <142491184+Oussamahammami2023@users.noreply.github.com>
2024-11-02 23:21:24 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,26 @@
 self.addEventListener('install', function(event) {
-  // تثبيت خدمة Worker
+  // تثبيت Service Worker وتخزين الملفات الأساسية
+  event.waitUntil(
+    caches.open('tnwork-cache-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'offline.html', // تأكد من وجود ملف offline.html
+        'image/app-icon-512x512.png'
+        // أضف باقي الملفات الأساسية التي تحتاج لتعمل بدون اتصال هنا
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      // إذا كان الملف موجودًا في الـ Cache، يتم عرضه مباشرة
+      return response || fetch(event.request).catch(function() {
+        // عند عدم توفر الملف في الـ Cache وعدم وجود اتصال بالإنترنت، يتم عرض offline.html
+        return caches.match('offline.html');
+      });
     })
   );
 });
