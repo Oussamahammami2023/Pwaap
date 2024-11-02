9ee5c9e3c2d97b382919eefe59c742f3143063cb
CME2024 <142491184+Oussamahammami2023@users.noreply.github.com>
2024-11-03 00:54:31 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,1 @@
-self.addEventListener('install', function(event) {
-  // تثبيت خدمة Worker
-});
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
