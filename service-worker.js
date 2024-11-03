0f00ced3b85dde6fb0660c11bb1e44d99f8e2b50
CME2024 <142491184+Oussamahammami2023@users.noreply.github.com>
2024-11-03 03:08:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,24 @@
+// Service Worker
+
 self.addEventListener('install', function(event) {
-  // تثبيت خدمة Worker
+  // إضافة صفحة offline.html إلى الذاكرة المؤقتة
+  event.waitUntil(
+    caches.open('offline-cache').then(function(cache) {
+      return cache.addAll([
+        'offline.html', // تأكد من أن المسار صحيح ويشير إلى ملف offline.html
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      // إذا كان الطلب موجودًا في الذاكرة المؤقتة، قم بإرجاعه
+      return response || fetch(event.request).catch(function() {
+        // إذا لم يكن هناك اتصال، قم بإرجاع صفحة offline.html من الذاكرة المؤقتة
+        return caches.match('offline.html');
+      });
     })
   );
 });
-
